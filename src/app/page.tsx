3896c1c66d707f43780015a100b2f13136440a2e
gumends <gustavo08.mendes@gmail.com>
2024-09-04 15:17:43 -0300

    vamp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,77 @@
-import Image from "next/image";
+'use client';
+import * as React from 'react';
+import Button from '@mui/joy/Button';
+import Stack from '@mui/joy/Stack';
+import Modal from '@mui/joy/Modal';
+import ModalClose from '@mui/joy/ModalClose';
+import ModalDialog, { ModalDialogProps } from '@mui/joy/ModalDialog';
+import DialogTitle from '@mui/joy/DialogTitle';
+import DialogContent from '@mui/joy/DialogContent';
+import { Avatar, Box, IconButton } from '@mui/joy';
+import FullscreenIcon from '@mui/icons-material/Fullscreen';
 
-export default function Home() {
+
+export default function LayoutModalDialog() {
+  const [layout, setLayout] = React.useState<ModalDialogProps['layout'] | undefined>(
+    undefined,
+  );
+
+  const [foto, setFoto] = React.useState('');
+
+  const imagens = [
+    {
+      "nome": "https://www.otempo.com.br/adobe/dynamicmedia/deliver/dm-aid--9e53280b-9307-43f0-8ac9-300129025684/entretenimento-vampeta-posa-em-sessao-de-fotos-para-a-revista-g-magazine-em-1999-1708766567.jpg?quality=90&preferwebp=true&width=1200"
+    },
+    {
+      "nome": "https://i.superesportes.com.br/_mNqaRdgVcZttj5yL6A62v7IZMU=/650x0/smart/imgsapp.mg.superesportes.com.br/app/noticia_126420360808/2023/01/04/3985307/vampeta-ex-jogador-da-selecao-brasileira_1_79005.jpg"
+    },
+    {
+      "nome": "https://i0.statig.com.br/bancodeimagens/ae/1g/8g/ae1g8gw1ijew1a8kauqebzdwd.jpg"
+    },
+    {
+      "nome": "https://scontent.fcgh39-1.fna.fbcdn.net/v/t39.30808-6/267366049_106047351935631_6446716623247923080_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=mX8NanOGtIwQ7kNvgFwYoO2&_nc_ht=scontent.fcgh39-1.fna&oh=00_AYC0Vfq8bnUNfTlpU4YyCEvEVM3fy2BGnwXFIJ1KXT4OiA&oe=66DE6776"
+    },
+    {
+      "nome": "https://jpimg.com.br/uploads/2022/04/vampeta-apontando.png"
+    },
+    {
+      "nome": "https://media.gettyimages.com/id/1237691195/pt/foto/corinthians-player-vampeta-gets-into-his-car-11-january-2000-in-sao-paulo-el-jugador-vampeta.jpg?s=1024x1024&w=gi&k=20&c=UMmHn1HTnByP_FsVjilPNAcokFlASE7FoHojuJlTtXo="
+    }
+  ]
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <React.Fragment>
+      <Box sx={{ width: '100vw', height: '100vh', display: 'flex', flexWrap: 'wrap', gap: 1 }}>
+        {
+          layout === 'fullscreen' ? null : imagens.map((imagem) => {
+            return (
+              <Box>
+                <Box sx={{ position: 'relative' }}>
+                  <img
+                    src={imagem.nome}
+                    alt="imagem"
+                    style={{ width: '100%', objectFit: 'cover' }}
+                  />
+                  <IconButton onClick={() => { setLayout('fullscreen'); setFoto(imagem.nome) }} sx={{ position: 'absolute', top: 0, right: 0, zIndex: 1, width: 40, height: 40 }}>
+                    <FullscreenIcon />
+                  </IconButton>
+                </Box>
+              </Box>
+            )
+          })
+        }
+      </Box>
+      <Modal
+        open={!!layout}
+        onClose={() => {setLayout(undefined);}}
+        sx={{ width: '100vw', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+      >
+        <img
+          src={foto}
+          alt="imagem"
+          style={{ width: '70%', height: '100%', objectFit: 'cover' }}
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </Modal>
+    </React.Fragment>
   );
 }
